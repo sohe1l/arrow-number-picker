6459a7befac1f7a0d242e1cf41d53ca49bd98f69
sohe1l <soheilansari@icloud.com>
2019-04-17 23:12:58 -0700

    added click listeners

--- arrownumberpicker/src/main/java/com/github/sohe1l/arrownumberpicker/ArrowNumberPicker.java
+++ arrownumberpicker/src/main/java/com/github/sohe1l/arrownumberpicker/ArrowNumberPicker.java
@@ -2,39 +2,70 @@ package com.github.sohe1l.arrownumberpicker;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.Dimension;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class ArrowNumberPicker extends FrameLayout {
 
 
+    private TextView tvNumber;
+    private TextView tvPlus;
+    private TextView tvMinus;
+
+    private int count = 0;
+
+    private Context context;
 
     private void init(Context context, @Nullable AttributeSet attrs){
+        this.context = context;
         LayoutInflater layoutInflater =  LayoutInflater.from(getContext());
         View view = layoutInflater.inflate(R.layout.arrow_number_picker, this);
 
+        tvNumber = findViewById(R.id.tv_number);
+        tvPlus = findViewById(R.id.tv_plus);
+        tvMinus = findViewById(R.id.tv_minus);
 
+        TypedArray attrTypedArray = context.obtainStyledAttributes(attrs, R.styleable.ArrowNumberPicker, 0, 0);
+        setAttrs(attrTypedArray);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ArrowNumberPicker, 0, 0);
+        setClickListeners();
+    }
+
+    private void setAttrs(TypedArray a){
         float textSize = a.getDimension(R.styleable.ArrowNumberPicker_textSize, context.getResources().getDimension(R.dimen.default_font_size));
         a.recycle();
-
-
-        TextView tvPicker = findViewById(R.id.picker_text_view);
-        tvPicker.setTextSize(textSize);
+        tvNumber.setTextSize(textSize);
 
     }
 
+    private void setClickListeners(){
+        tvPlus.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count ++;
+                updateCounter();
+            }
+        });
+
+        tvMinus.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(count>0){
+                    count --;
+                    updateCounter();
+                }
+            }
+        });
+    }
+
+    private void updateCounter(){
+        tvNumber.setText(String.valueOf(count));
+    }
 
     public ArrowNumberPicker(Context context) {
         super(context);
@@ -51,11 +82,6 @@ public class ArrowNumberPicker extends FrameLayout {
     public ArrowNumberPicker(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context, attrs);
-
-
-
-
-
     }
 
 //
