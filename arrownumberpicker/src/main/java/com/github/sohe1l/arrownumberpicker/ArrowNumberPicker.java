0ffd201e1ff2f696a6a12b52a8d6a40ad35a5201
sohe1l <soheilansari@icloud.com>
2019-04-17 22:13:19 -0700

    Added template files and config

--- arrownumberpicker/src/main/java/com/github/sohe1l/arrownumberpicker/ArrowNumberPicker.java
+++ arrownumberpicker/src/main/java/com/github/sohe1l/arrownumberpicker/ArrowNumberPicker.java
@@ -1,6 +1,7 @@
 package com.github.sohe1l.arrownumberpicker;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
@@ -8,7 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.Dimension;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -16,27 +19,43 @@ public class ArrowNumberPicker extends FrameLayout {
 
 
 
-    private void init(@Nullable AttributeSet attrs){
+    private void init(Context context, @Nullable AttributeSet attrs){
         LayoutInflater layoutInflater =  LayoutInflater.from(getContext());
-        layoutInflater.inflate(R.layout.arrow_number_picker, this);
+        View view = layoutInflater.inflate(R.layout.arrow_number_picker, this);
+
+
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ArrowNumberPicker, 0, 0);
+        float textSize = a.getDimension(R.styleable.ArrowNumberPicker_textSize, context.getResources().getDimension(R.dimen.default_font_size));
+        a.recycle();
+
+
+        TextView tvPicker = findViewById(R.id.picker_text_view);
+        tvPicker.setTextSize(textSize);
+
     }
 
 
     public ArrowNumberPicker(Context context) {
         super(context);
-        init(null);
+        init(context, null);
     }
 
 
     public ArrowNumberPicker(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(attrs);
+        init(context, attrs);
     }
 
 
     public ArrowNumberPicker(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs);
+        init(context, attrs);
+
+
+
+
+
     }
 
 //
